ec33055b41a811a5679ffcc44ef35f9fa9d2cebe
Kramskiy Dima <kramskiyd@gmail.com>
2021-03-14 11:48:27 +0200

    add react app with image finder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Searchbar from "./components/Searchbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Searchbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+const Searchbar = () => {
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm">
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
+
+        <input
+          className="SearchForm-input"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
+
+export default Searchbar;
