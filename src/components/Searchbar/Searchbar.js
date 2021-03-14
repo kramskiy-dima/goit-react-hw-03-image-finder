e3379d16fc610637befc09084c0765d9e877c48c
Kramskiy Dima <kramskiyd@gmail.com>
2021-03-14 21:50:58 +0200

    create react app for search image

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,23 +1,51 @@
 import React, { Component } from "react";
+import s from "./Searchbar.module.css";
+import PropTypes from "prop-types";
 
-const Searchbar = () => {
-  return (
-    <header className="Searchbar">
-      <form className="SearchForm">
-        <button type="submit" className="SearchForm-button">
-          <span className="SearchForm-button-label">Search</span>
-        </button>
+class Searchbar extends Component {
+  state = {
+    queryValue: "",
+  };
 
-        <input
-          className="SearchForm-input"
-          type="text"
-          autocomplete="off"
-          autofocus
-          placeholder="Search images and photos"
-        />
-      </form>
-    </header>
-  );
+  hendleChange = (e) => {
+    this.setState({
+      queryValue: e.currentTarget.value,
+    });
+  };
+
+  hendleSubmit = (e) => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.queryValue);
+    this.setState({ queryValue: "" });
+  };
+
+  render() {
+    return (
+      <header className={s.Searchbar}>
+        <form className={s.SearchForm} onSubmit={this.hendleSubmit}>
+          <button type="submit" className={s["SearchForm-button"]}>
+            <span className={s["SearchForm-button-label"]}>Search</span>
+          </button>
+
+          <input
+            className={s["SearchForm-input"]}
+            onChange={this.hendleChange}
+            value={this.state.queryValue}
+            name="query"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default Searchbar;
